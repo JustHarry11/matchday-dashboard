d453e67a5155941fe9d99a09c3bfdfcdca126d47
Harry <harrylippa@gmail.com>
2026-01-14 00:39:26 +0000

    Removed other API routes

--- src/pages/api/team.ts
+++
@@ -1,9 +0,0 @@
-import { getTeams } from "@/lib/api";
-
-export async function GET() {
-  const teams = await getTeams();
-  return new Response(JSON.stringify({ teams }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
